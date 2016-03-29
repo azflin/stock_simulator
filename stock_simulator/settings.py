93d0cea458fca65a1fe91c5074437b9fa17d79a3
Allen Lin <allenlin1992@hotmail.com>
2016-03-29 00:00:08 -0400

    renaming

--- stock_simulator/settings.py
+++ stock_simulator/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'stock_simulator_project.urls'
+ROOT_URLCONF = 'stock_simulator.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'stock_simulator_project.wsgi.application'
+WSGI_APPLICATION = 'stock_simulator.wsgi.application'
 
 
 # Database
